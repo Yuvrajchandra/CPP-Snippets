8567f4d9314538c308cad6fab2e0514d8020ad67
YuvrajChandra <Singhyuvraj179@gmail.com>
2022-04-19 09:59:15 +0530

    size of vector

---
+++ 1-size-of-vector.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector <int> arr1 = {1, 2, 3, 4};
+    vector <int> arr2 = {};
+    vector <float> arr3 = {1.2, 3.8, 3.0, 2.7, 6.6};
+
+    cout << "Size of arr1: " << arr1.size() << endl;
+    cout << "Size of arr2: " << arr2.size() << endl;
+    cout << "Size of arr3: " << arr3.size() << endl;
+
+    return 0;
+}
