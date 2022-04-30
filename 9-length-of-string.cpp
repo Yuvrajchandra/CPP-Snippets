1fe926ce9fe1eba5e14ed2e477796ed1dc774667
YuvrajChandra <Singhyuvraj179@gmail.com>
2022-04-30 10:32:09 +0530

    find length of string

---
+++ 9-length-of-string.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string str1 = "MakeUseOf";
+    cout << "Length of " << str1 << " : " << str1.length() << endl;
+
+    string str2 = "lorem ipsum";
+    cout << "Length of " << str2 << " : " << str2.size() << endl;
+
+    return 0;
+}
